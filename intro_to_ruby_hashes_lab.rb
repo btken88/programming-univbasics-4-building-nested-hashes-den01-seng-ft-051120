9af1312b0078e280413d328b9244fde5894db220
Bryce Kennedy <btken88@gmail.com>
2020-04-16 22:11:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ def monopoly_with_third_tier
       one_piece_owned: 25,
       two_pieces_owned: 50,
       three_pieces_owned: 100,
-      three_pieces_owned: 200
+      four_pieces_owned: 200
     },
     names: {
       reading_railroad: {},
@@ -46,7 +46,7 @@ def monopoly_with_fourth_tier
       one_piece_owned: 25,
       two_pieces_owned: 50,
       three_pieces_owned: 100,
-      three_pieces_owned: 200
+      four_pieces_owned: 200
     },
     names: {
       reading_railroad: {mortgage_value: 100},
